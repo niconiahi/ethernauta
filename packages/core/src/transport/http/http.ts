26692a776df324ad92abfd3c23f76d56daf7e041
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-11-28 21:14:10 -0300

    feat(transport/http): uses validation schemas to infer types

--- packages/core/src/transport/http/http.ts
+++ packages/core/src/transport/http/http.ts
@@ -1,122 +1,64 @@
+import { Input, array, literal, number, object, optional, record, string, union, unknown, keyof, includes } from "valibot";
 import type { JsonRpcCall } from "../../provider";
 
 // https://www.jsonrpc.org/specification#parameter_structures
-type JsonRpcParameters =
-  | unknown[]
-  | {
-      [name: string]: unknown;
-    };
+const jsonRpcSchema = union([array(unknown()), record(unknown())])
+type JsonRpcParameters = Input<typeof jsonRpcSchema>;
 // https://www.jsonrpc.org/specification#request_object
-type JsonRpcRequest = {
-  jsonrpc: "2.0";
-  method: string;
-  params?: JsonRpcParameters;
-  // An identifier established by the Client that MUST contain a String, Number, or NULL value if included.
-  id: string | number;
-};
+const jsonRpcRequest = object({
+  jsonrpc: literal('2.0'),
+  method: string(),
+  params: optional(jsonRpcSchema),
+  id: union([string(), number()])
+})
+type JsonRpcRequest = Input<typeof jsonRpcRequest>
 // https://www.jsonrpc.org/specification#notification
-type JsonRpcNotification = {
-  jsonrpc: "2.0";
-  method: string;
-
-  params?: JsonRpcParameters
-  // id: If it is not included it is assumed to be a notification
-};
+const jsonRpcNotification = object({
+  jsonrpc: literal('2.0'),
+  method: string(),
+  params: optional(jsonRpcSchema),
+})
+type JsonRpcNotification = Input<typeof jsonRpcNotification>
 // https://www.jsonrpc.org/specification#error_object
-const JSON_RPC_REPONSE_ERROR_CODE = {
-  PARSE_ERROR: 32700,
-  INVALID_REQUEST: 32600,
-  METHOD_NOT_FOUND: 32601,
-  INVALID_PARAMS: 32602,
-  INTERNAL_ERROR: 32603,
-} as const;
-type ObjectValues<T> = T[keyof T];
-type JsonRpcResponseErrorCode = ObjectValues<
-  typeof JSON_RPC_REPONSE_ERROR_CODE
->;
-// https://www.jsonrpc.org/specification#error_object
-interface JsonRpcResponseError extends Error {
-  code: JsonRpcResponseErrorCode;
-  message: string;
-  data?: unknown;
-}
+const jsonRpcResponseError = object({
+  data: optional(unknown()),
+  message: string(),
+  code: union([
+    literal(32700),
+    literal(32701),
+    literal(32702),
+    literal(32600),
+    literal(32601),
+    literal(32602),
+    literal(32603),
+    literal(32500),
+    literal(32400),
+    literal(32300)
+  ]),
+})
 // https://www.jsonrpc.org/specification#response_object
-export type JsonRpcFailedResponse = {
-  jsonrpc: "2.0";
-  error: JsonRpcResponseError;
-  // result: This member MUST NOT exist if there was an error invoking the method.
-};
-export type JsonRpcSuccesfulResponse = {
-  jsonrpc: "2.0";
-  result: unknown;
-  // error: This member MUST NOT exist if there was no error triggered during invocation.
-};
-
-// https://www.jsonrpc.org/specification#extensions
-function validateMethodName(name: string) {
-  if (name.startsWith("rpc.")) {
-    throw new Error(
-      `"rpc." prefixed methods are reserved for system extensions. More info at: https://www.jsonrpc.org/specification#extensions`,
-    );
-  }
-}
-
-// https://www.jsonrpc.org/specification#request_object
-export function runJsonRpcRequest(
-  request: JsonRpcRequest,
-): JsonRpcFailedResponse | JsonRpcSuccesfulResponse {
-  validateMethodName(request.method);
-
-  const successful: JsonRpcSuccesfulResponse = {
-    jsonrpc: "2.0",
-    result: "something",
-  };
-
-  return successful;
-}
-// https://www.jsonrpc.org/specification#notification
-export function runJsonRpcNotification(
-  notification: JsonRpcNotification,
-): JsonRpcFailedResponse | JsonRpcSuccesfulResponse {
-  validateMethodName(notification.method);
-
-  const successful: JsonRpcSuccesfulResponse = {
-    jsonrpc: "2.0",
-    result: "something",
-  };
-
-  return successful;
-}
-
-// https://www.jsonrpc.org/specification#batch
-export function runJsonRpcBatch(
-  batch: (JsonRpcRequest | JsonRpcNotification)[],
-): (JsonRpcFailedResponse | JsonRpcSuccesfulResponse)[] | JsonRpcFailedResponse {
-  batch.every((call) => validateMethodName(call.method));
-
-  const successful: JsonRpcSuccesfulResponse = {
-    jsonrpc: "2.0",
-    result: "something",
-  };
-  const error: JsonRpcFailedResponse = {
-    jsonrpc: "2.0",
-    error: {
-      code: 32600,
-      message: "omg",
-      name: "_omg",
-    },
-  };
-
-  return [successful, error];
-}
-
+const jsonRpcFailedResponse = object({
+  jsonrpc: literal('2.0'),
+  error: jsonRpcResponseError
+})
+type JsonRpcFailedResponse = Input<typeof jsonRpcFailedResponse>
+const jsonRpcSuccesfulResponse = object({
+  jsonrpc: literal('2.0'),
+  result: unknown()
+})
+type JsonRpcSuccesfulResponse = Input<typeof jsonRpcSuccesfulResponse>
+const methodNameSchema = includes("rpc.")
 export function httpTransport(
   url: string,
 ): (call: JsonRpcCall) => Promise<JsonRpcFailedResponse | JsonRpcSuccesfulResponse> {
   async function transport(
     call: JsonRpcCall,
   ): Promise<JsonRpcFailedResponse | JsonRpcSuccesfulResponse> {
-    const request: JsonRpcRequest = {jsonrpc: '2.0', id: 'SOME_RANDOM_GENERATED_ID',method: call[0], params: call[1] }
+    const [method, params] = call
+
+    methodNameSchema._parse(method)
+
+    const request: JsonRpcRequest = {jsonrpc: '2.0', id: 'SOME_RANDOM_GENERATED_ID',method, params}
     const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(request),
@@ -131,3 +73,54 @@ export function httpTransport(
   return transport;
 }
 export type HttpTransport = ReturnType<typeof httpTransport>;
+
+// https://www.jsonrpc.org/specification#extensions
+
+// // https://www.jsonrpc.org/specification#request_object
+// function runJsonRpcRequest(
+//   request: JsonRpcRequest,
+// ): JsonRpcFailedResponse | JsonRpcSuccesfulResponse {
+//   validateMethodName(request.method);
+
+//   const successful: JsonRpcSuccesfulResponse = {
+//     jsonrpc: "2.0",
+//     result: "something",
+//   };
+
+//   return successful;
+// }
+// // https://www.jsonrpc.org/specification#notification
+// function runJsonRpcNotification(
+//   notification: JsonRpcNotification,
+// ): JsonRpcFailedResponse | JsonRpcSuccesfulResponse {
+//   validateMethodName(notification.method);
+
+//   const successful: JsonRpcSuccesfulResponse = {
+//     jsonrpc: "2.0",
+//     result: "something",
+//   };
+
+//   return successful;
+// }
+
+// // https://www.jsonrpc.org/specification#batch
+// function runJsonRpcBatch(
+//   batch: (JsonRpcRequest | JsonRpcNotification)[],
+// ): (JsonRpcFailedResponse | JsonRpcSuccesfulResponse)[] | JsonRpcFailedResponse {
+//   batch.every((call) => validateMethodName(call.method));
+
+//   const successful: JsonRpcSuccesfulResponse = {
+//     jsonrpc: "2.0",
+//     result: "something",
+//   };
+//   const error: JsonRpcFailedResponse = {
+//     jsonrpc: "2.0",
+//     error: {
+//       code: 32600,
+//       message: "omg",
+//       data: "some extra data",
+//     },
+//   };
+
+//   return [successful, error];
+// }
