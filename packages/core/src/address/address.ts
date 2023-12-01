30f43d223494e103c3da45c63b4f7b811001f33f
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-12-01 13:44:53 -0300

    feat(address): add module

--- packages/core/src/address/address.ts
+++ packages/core/src/address/address.ts
@@ -1,7 +1,8 @@
-import { Input, special } from "valibot";
+import { Input, safeParse, special } from "valibot";
+import { hexadecimalSchema } from "@ethernauta/core";
 
-function isAddress(input: unknown)  {
-  return typeof input === 'string' && /^0x[a-fA-F0-9]{40}$/.test(input);
+function isAddress(input: unknown) {
+  return safeParse(hexadecimalSchema, input).success
 }
 export const addressSchema = special<`0x${string}`>(isAddress)
 export type Address = Input<typeof addressSchema>
