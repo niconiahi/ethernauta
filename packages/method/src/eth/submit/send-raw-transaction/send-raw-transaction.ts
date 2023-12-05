fdeecdeb62736b4ff52a8974fb92ff61c710808c
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-12-05 12:51:09 -0300

    feat(method/submit): use writer generic

--- packages/method/src/eth/submit/send-raw-transaction/send-raw-transaction.ts
+++ packages/method/src/eth/submit/send-raw-transaction/send-raw-transaction.ts
@@ -1,6 +1,6 @@
 import type { Hash32 } from "@ethernauta/core"
 import { bytesSchema, hash32Schema } from "@ethernauta/core"
-import type { Writer } from "@ethernauta/transport"
+import type { Writable, Writer } from "@ethernauta/transport"
 import { callSchema } from "@ethernauta/transport"
 import type { Input } from "valibot"
 import { parse, tuple } from "valibot"
@@ -12,16 +12,16 @@ type Parameters = Input<typeof parametersSchema>
  * @param transaction The transaction to be sent
  * @returns The transaction hash
  */
-export async function sendRawTransaction(writer: Writer, _parameters: Parameters): Promise<Hash32> {
-  const method = "eth_sendRawTransaction"
-  const parameters = parse(parametersSchema, _parameters)
-  const call = parse(callSchema, [method, parameters])
-  const response = await writer(call)
-  if ("error" in response) {
-    throw new Error(response.error.message)
+export function sendRawTransaction(_parameters: Parameters): Writable<Hash32> {
+  return async (writer: Writer): Promise<Hash32> => {
+    const method = "eth_sendRawTransaction"
+    const parameters = parse(parametersSchema, _parameters)
+    const call = parse(callSchema, [method, parameters])
+    const response = await writer(call)
+    if ("error" in response) {
+      throw new Error(response.error.message)
+    }
+    const result = parse(hash32Schema, response.result)
+    return result
   }
-
-  const result = parse(hash32Schema, response.result)
-
-  return result
 }
--- packages/method/src/eth/submit/send-transaction/send-transaction.ts
+++ packages/method/src/eth/submit/send-transaction/send-transaction.ts
@@ -1,6 +1,6 @@
 import type { Hash32 } from "@ethernauta/core"
 import { genericTransactionSchema, hash32Schema } from "@ethernauta/core"
-import type { Writer } from "@ethernauta/transport"
+import type { Writable, Writer } from "@ethernauta/transport"
 import { callSchema } from "@ethernauta/transport"
 import type { Input } from "valibot"
 import { parse, tuple } from "valibot"
@@ -12,16 +12,16 @@ type Parameters = Input<typeof parametersSchema>
  * @param transaction The transaction object to be sent.
  * @returns The transaction hash as a promise.
  */
-export async function sendTransaction(writer: Writer, _parameters: Parameters): Promise<Hash32> {
-  const method = "eth_sendTransaction"
-  const parameters = parse(parametersSchema, _parameters)
-  const call = parse(callSchema, [method, parameters])
-  const response = await writer(call)
-  if ("error" in response) {
-    throw new Error(response.error.message)
+export function sendTransaction(_parameters: Parameters): Writable<Hash32> {
+  return async (writer: Writer): Promise<Hash32> => {
+    const method = "eth_sendTransaction"
+    const parameters = parse(parametersSchema, _parameters)
+    const call = parse(callSchema, [method, parameters])
+    const response = await writer(call)
+    if ("error" in response) {
+      throw new Error(response.error.message)
+    }
+    const result = parse(hash32Schema, response.result)
+    return result
   }
-
-  const result = parse(hash32Schema, response.result)
-
-  return result
 }
