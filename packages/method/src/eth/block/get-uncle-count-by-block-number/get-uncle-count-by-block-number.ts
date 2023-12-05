9d98a3ba0f90840edc4c96fb47fe6067451c6107
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-12-05 15:56:56 -0300

    feat(method/block): add get uncle count methods

---
+++ packages/method/src/eth/block/get-uncle-count-by-block-number/get-uncle-count-by-block-number.ts
@@ -0,0 +1,27 @@
+import type { NotFound, Uint } from "@ethernauta/core"
+import { blockNumberOrTag, notFoundSchema, uintSchema } from "@ethernauta/core"
+import type { Readable, Reader } from "@ethernauta/transport"
+import { callSchema } from "@ethernauta/transport"
+import type { Input } from "valibot"
+import { parse, tuple, union } from "valibot"
+
+const parametersSchema = tuple([blockNumberOrTag])
+type Parameters = Input<typeof parametersSchema>
+/**
+ * Returns the number of transactions in a block from a block matching the given block hash
+ * @param blockHash The block hash in which to search
+ * @returns The transaction count or null if not found
+ */
+export function getUncleCountByBlockNumber(_parameters: Parameters): Readable<Uint | NotFound> {
+  return async (reader: Reader): Promise<Uint | NotFound> => {
+    const method = "eth_getUncleCountByBlockNumber"
+    const parameters = parse(parametersSchema, _parameters)
+    const call = parse(callSchema, [method, parameters])
+    const response = await reader(call)
+    if ("error" in response) {
+      throw new Error(response.error.message)
+    }
+    const result = parse(union([uintSchema, notFoundSchema]), response.result)
+    return result
+  }
+}
