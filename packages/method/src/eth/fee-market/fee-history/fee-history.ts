394b7534b5c2539cf5d70fc3dc79606732eaa9fb
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-12-16 15:21:17 -0300

    feat(method/fee-market): accept parameters as object

--- packages/method/src/eth/fee-market/fee-history/fee-history.ts
+++ packages/method/src/eth/fee-market/fee-history/fee-history.ts
@@ -1,12 +1,19 @@
-import { blockNumberOrTag, ratioSchema, uintSchema } from "@ethernauta/core"
+import { blockNumberOrTagSchema, ratioSchema, uintSchema } from "@ethernauta/core"
 import type { Readable, Reader } from "@ethernauta/transport"
 import { callSchema } from "@ethernauta/transport"
 import type { Input } from "valibot"
-import { array, maxValue, minValue, number, object, parse, tuple } from "valibot"
+import { array, maxValue, minValue, number, object, parse, tuple, union } from "valibot"
 
 const percentileSchema = number([minValue(0), maxValue(100)])
 const rewardsSchema = array(percentileSchema)
-const parametersSchema = tuple([uintSchema, blockNumberOrTag, array(percentileSchema)])
+const parametersSchema = union([
+  tuple([uintSchema, blockNumberOrTagSchema, array(percentileSchema)]),
+  object({
+    blockCount: uintSchema,
+    newestBlock: blockNumberOrTagSchema,
+    rewardPercentiles: rewardsSchema,
+  }),
+])
 type Parameters = Input<typeof parametersSchema>
 const feeHistoryResultsSchema = object({
   oldestBlock: uintSchema,
@@ -17,9 +24,6 @@ const feeHistoryResultsSchema = object({
 export type FeeHistoryResults = Input<typeof feeHistoryResultsSchema>
 /**
  * Returns code at a given address
- * @param blockCount Requested range of blocks. Clients will return less than the requested range if not all blocks are available
- * @param newestBlock Highest block of the requested range
- * @param rewardPercentiles A monotonically increasing list of percentile values. For each block in the requested range, the transactions will be sorted in ascending order by effective tip per gas and the coresponding effective tip for the percentile will be determined, accounting for gas consumed
  * @returns Fee history for the returned block range. This can be a subsection of the requested range if not all blocks are available
  */
 export function feeHistory(_parameters: Parameters): Readable<FeeHistoryResults> {
