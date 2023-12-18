cd90aa511c1060bc6324eec3353523ca5e089c09
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-12-18 09:50:15 -0300

    test(abi/json): separate tests between types

--- packages/abi/src/json/json.test.ts
+++ packages/abi/src/json/json.test.ts
@@ -4,8 +4,15 @@ import { getFunctions, getInputs } from "../json"
 
 import erc20 from "./abi/erc20.abi.json"
 
-describe("extract", () => {
-  it("should correctly extract erc20 from json", async () => {
+describe("getInputs", () => {
+  it("should correctly extract erc20 inputs", async () => {
+    const inputs = getInputs(erc20)
+    console.log("it => inputs =>", inputs)
+  })
+})
+
+describe("getFunctions", () => {
+  it("should correctly extract erc20 functions", async () => {
     const inputs = getInputs(erc20)
     const functions = getFunctions(inputs)
 
