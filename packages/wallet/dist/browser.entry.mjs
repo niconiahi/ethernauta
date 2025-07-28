6b571dd15d7cf99d77bbb40e3c5f4a2b9afe72ae
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-07-28 14:48:29 -0300

    chore: untrack dist folder

--- packages/wallet/dist/browser.entry.mjs
+++
@@ -1,7 +0,0 @@
-const t = document.createElement("script");
-t.src = chrome.runtime.getURL("cryptoman.mjs");
-t.onload = () => t.remove();
-document.head.appendChild(t);
-window.addEventListener("message", (e) => {
-  e.source === window && (console.log("event.data", e.data), e.data?.type?.startsWith("CRYPTOMAN_") && chrome.runtime.sendMessage(e.data));
-});
