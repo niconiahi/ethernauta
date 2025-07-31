7a63c670bdd3258c87fe366952c57988e84e0c1c
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-07-31 19:06:48 -0300

    feat: signature happens on button click

--- packages/wallet/src/views/sign/index.tsx
+++ packages/wallet/src/views/sign/index.tsx
@@ -1,32 +1,9 @@
-import { useEffect } from "preact/hooks"
 import { transaction_request } from "../../utils/transaction"
 import { sign_transaction } from "../../utils/sign-transaction"
 import { bytes_to_hex } from "../../utils/hex"
-import {
-  CryptomanResponseSchema,
-  type CryptomanResponse,
-} from "../../utils/event"
-import * as v from "valibot"
+import type { CryptomanResponse } from "../../utils/event"
 
 export function Sign() {
-  useEffect(() => {
-    async function run() {
-      const signed_transaction = await sign_transaction(
-        transaction_request.value.method,
-        transaction_request.value.params,
-      )
-      console.log("signed_transaction", signed_transaction)
-      const response: CryptomanResponse = {
-        id: transaction_request.value.id,
-        type: "CRYPTOMAN_RESPONSE_SIGNED_TRANSACTION",
-        signed_transaction: bytes_to_hex(
-          signed_transaction,
-        ),
-      }
-      chrome.runtime.sendMessage(response)
-    }
-    run()
-  }, [])
   return (
     <div>
       <h1>you are about to sign</h1>
@@ -39,6 +16,29 @@ export function Sign() {
           2,
         )}
       </p>
+      <button
+        type="button"
+        onClick={async () => {
+          const signed_transaction = await sign_transaction(
+            transaction_request.value.method,
+            transaction_request.value.params,
+          )
+          console.log(
+            "signed_transaction",
+            signed_transaction,
+          )
+          const response: CryptomanResponse = {
+            id: transaction_request.value.id,
+            type: "CRYPTOMAN_RESPONSE_SIGNED_TRANSACTION",
+            signed_transaction: bytes_to_hex(
+              signed_transaction,
+            ),
+          }
+          chrome.runtime.sendMessage(response)
+        }}
+      >
+        sign
+      </button>
     </div>
   )
 }
