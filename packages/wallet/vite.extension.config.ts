e8b969e27871a4b7e7ab57fc138bdec9efc61b5f
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-08-01 14:17:48 -0300

    chore: simplify the development command under a single dev command

--- packages/wallet/vite.extension.config.ts
+++ packages/wallet/vite.extension.config.ts
@@ -6,43 +6,37 @@ import { defineConfig } from "vite"
 const package_json = JSON.parse(
   readFileSync("./package.json", "utf-8"),
 )
-export default defineConfig(({ command }) => {
-  console.log("command", command)
-  return {
-    plugins: [preact(), tsconfigPaths()],
-    root: "src",
-    publicDir: "../public",
-    build: {
-      outDir: "../dist",
-      // emptyOutDir: true,
-      rollupOptions: {
-        output: {
-          format: "es",
-          entryFileNames:
-            command === "build"
-              ? "assets/[name].mjs"
-              : "assets/[name]-[hash].mjs",
-          chunkFileNames:
-            command === "build"
-              ? "assets/[name].mjs"
-              : "assets/[name]-[hash].mjs",
-          assetFileNames:
-            command === "build"
-              ? "assets/[name].[ext]"
-              : "assets/[name]-[hash].[ext]",
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              if (id.includes("preact-robot"))
-                return `preact-robot-${package_json.dependencies["preact-robot"]}`
-              if (id.includes("preact"))
-                return `preact-${package_json.dependencies.preact}`
-              if (id.includes("robot3"))
-                return `robot3-${package_json.dependencies.robot3}`
-              return "vendor"
-            }
-          },
+export default defineConfig({
+  plugins: [preact(), tsconfigPaths()],
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    // emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        format: "es",
+        entryFileNames: process.argv.includes("--watch")
+          ? "assets/[name].mjs"
+          : "assets/[name]-[hash].mjs",
+        chunkFileNames: process.argv.includes("--watch")
+          ? "assets/[name].mjs"
+          : "assets/[name]-[hash].mjs",
+        assetFileNames: process.argv.includes("--watch")
+          ? "assets/[name].[ext]"
+          : "assets/[name]-[hash].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("preact-robot"))
+              return `preact-robot-${package_json.dependencies["preact-robot"]}`
+            if (id.includes("preact"))
+              return `preact-${package_json.dependencies.preact}`
+            if (id.includes("robot3"))
+              return `robot3-${package_json.dependencies.robot3}`
+            return "vendor"
+          }
         },
       },
     },
-  }
+  },
 })
