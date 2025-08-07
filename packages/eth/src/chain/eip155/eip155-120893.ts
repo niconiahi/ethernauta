53a24c2ce8165731b270609932aeeafe9ed4f739
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-08-07 11:59:26 -0300

    chore: merge chain into eth package

---
+++ packages/eth/src/chain/eip155/eip155-120893.ts
@@ -0,0 +1,29 @@
+/* eslint no-template-curly-in-string: 0 */
+import type { Chain } from "../shared"
+
+export const eip155_120893 = {
+  "name": "Sova Sepolia Testnet",
+  "shortName": "sovasep",
+  "chain": "ETH",
+  "icon": "sova",
+  "rpc": [
+    "https://rpc.testnet.sova.io"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Sepolia Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "infoURL": "https://sova.io",
+  "chainId": 120893,
+  "networkId": 120893,
+  "slip44": 1,
+  "explorers": [
+    {
+      "name": "blockscout",
+      "url": "https://explorer.testnet.sova.io",
+      "standard": "EIP3091"
+    }
+  ]
+} satisfies Chain
