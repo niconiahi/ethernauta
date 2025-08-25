5d3d162220bc8ccdc012be08f5c5d46b5e882a95
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-08-25 12:29:21 -0300

    test: add snapshot test case

--- packages/eth/src/erc/20/20.test.ts
+++ packages/eth/src/erc/20/20.test.ts
@@ -28,49 +28,48 @@ describe("ERC20", () => {
       expect(generated_files).toContain(file_name)
     }
   })
-  //   it("should create the correct total supply", () => {
-  //     const descriptions = parse(
-  //       array(DescriptionSchema),
-  //       ERC20_ABI,
-  //     )
-  //     const functions = descriptions.filter((description) => {
-  //       return description.type === "function"
-  //     })
-  //     const this_dir = this_directory(import.meta.url)
-  //     compile(functions, this_dir)
-  //     const methods_dir = join(this_dir, "methods")
-  //     execSync(`biome format --write ${methods_dir}/*.ts`)
-  //     const SNAPSHOT = `
-  // import type { Http, Readable } from "@ethernauta/transport"
-  // import { callSchema } from "@ethernauta/transport"
-  // import { parse, union } from "valibot"
-  //
-  // import { uint256Schema } from "@ethernauta/eth"
-  // import type { Uint256 } from "@ethernauta/eth
-  //
-  // export function totalSupply(): Readable<Uint256> {
-  //   return async (transports: Http[]): Promise<Uint256> => {
-  //     const method = "totalSupply"
-  //     const call = parse(callSchema, [method, []])
-  //     const response = await Promise.any(
-  //       transports.map((transport) => transport(call)),
-  //     )
-  //     if ("error" in response) {
-  //       throw new Error(response.error.message)
-  //     }
-  //     const result = parse(
-  //       union([uint256Schema]),
-  //       response.result,
-  //     )
-  //     return result
-  //   }
-  // }`
-  //     const path = join(
-  //       this_dir,
-  //       "methods",
-  //       "total-supply.ts",
-  //     )
-  //     const content = readFileSync(path, { encoding: "utf8" })
-  //     expect(content.trim()).toBe(SNAPSHOT.trim())
-  //   })
+  it("should create the correct total supply", () => {
+    const descriptions = parse(
+      array(DescriptionSchema),
+      ERC20_ABI,
+    )
+    const functions = descriptions.filter((description) => {
+      return description.type === "function"
+    })
+    const this_dir = this_directory(import.meta.url)
+    compile(functions, this_dir)
+    const methods_dir = join(this_dir, "methods")
+    execSync(`biome format --write ${methods_dir}/*.ts`)
+    const SNAPSHOT = `
+import type { Http, Readable } from "@ethernauta/transport"
+import { callSchema } from "@ethernauta/transport"
+import { parse, union } from "valibot"
+import { uint256Schema } from "@ethernauta/eth"
+import type { Uint256 } from "@ethernauta/eth"
+
+export function totalSupply(): Readable<Uint256> {
+  return async (transports: Http[]): Promise<Uint256> => {
+    const method = "totalSupply"
+    const call = parse(callSchema, [method, []])
+    const response = await Promise.any(
+      transports.map((transport) => transport(call)),
+    )
+    if ("error" in response) {
+      throw new Error(response.error.message)
+    }
+    const result = parse(
+      union([uint256Schema]),
+      response.result,
+    )
+    return result
+  }
+}`
+    const path = join(
+      this_dir,
+      "methods",
+      "total-supply.ts",
+    )
+    const content = readFileSync(path, { encoding: "utf8" })
+    expect(content.trim()).toBe(SNAPSHOT.trim())
+  })
 })
