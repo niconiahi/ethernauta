8470bf61a49a142ba1e02bb676b48f1af2ac0a33
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-11-13 20:05:47 -0300

    feat: add privacy policy route

---
+++ examples/playground/app/routes/privacy-policy.tsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <p>
+      This application does not collect any information. All
+      data stored in only stored locally in the user's
+      browser
+    </p>
+  )
+}
