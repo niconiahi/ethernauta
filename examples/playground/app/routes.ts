49c0b5975a0481e1143036215ebb76f1d3757144
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-11-13 20:16:25 -0300

    fix: add privacy policyt route config

--- examples/playground/app/routes.ts
+++ examples/playground/app/routes.ts
@@ -1,8 +1,10 @@
 import {
   index,
+  route,
   type RouteConfig,
 } from "@react-router/dev/routes"
 
 export default [
-  index("routes/home.tsx"),
+  index("./routes/home.tsx"),
+  route("privacy-policy", "./routes/privacy-policy.tsx"),
 ] satisfies RouteConfig
