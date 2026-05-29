50bcf32d7ce6b0b769fe2618f668e551ed5c3afb
Nicolas Accetta <nicolas.accetta@gmail.com>
2026-05-29 15:50:27 -0300

    feat(ens): expose find_resolver as part of the public resolution surface

--- packages/ens/src/resolution/index.ts
+++ packages/ens/src/resolution/index.ts
@@ -1,5 +1,10 @@
 // ENS resolution orchestration (see PAPER.md).
 
+export {
+  type FindResolverResult,
+  FindResolverResultSchema,
+  find_resolver,
+} from "./find-resolver"
 export { get_ens_address } from "./get-ens-address"
 export { get_ens_avatar } from "./get-ens-avatar"
 export { get_ens_name } from "./get-ens-name"
